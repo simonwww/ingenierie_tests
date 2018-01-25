cc3d0e53cb35ce8975b7f3daf2ed8a57adc6a24c
unknown <tom.wang1994@gmail.com>
2018-01-25 22:49:05 +0100

    ajout tests

--- test/unit/IntervalTest.js
+++ test/unit/IntervalTest.js
@@ -35,4 +35,137 @@ describe("nouveau test",function(){
 	});
 });
 
+describe("Interval - including", function () {
+    testedInterval = new Interval(10, 20);
+
+    [
+        
+        new Interval(15, 16),
+        new Interval(12, 19),
+        new Interval(10, 20)
+    ].forEach(function (interval) {
+        it("should includes " + testedInterval.toString() + " and " + interval.toString(), function () {
+            expect(testedInterval.includes(interval)).toBeTruthy();
+        });
+    });
+
+    [
+		new Interval(8, 12),
+		new Interval(17, 22),
+        new Interval(8, 9),
+		new Interval(8, 21),
+        new Interval(21, 22)
+    ].forEach(function (interval) {
+        it("should not includes " + testedInterval.toString() + " and " + interval.toString(), function () {
+            expect(testedInterval.includes(interval)).toBeFalsy();
+        });
+    });
+});
+
+describe("Interval - union", function () {
+    testedInterval = new Interval(10, 20);
+
+    [
+		{
+			interval : new Interval(8, 12),
+			result : new Interval(8,20)
+		},
+		
+		{
+			interval : new Interval(8, 9),
+			result : [testedInterval, new Interval(8,20)]
+		},
+		{
+			interval : new Interval(17, 22),
+			result : new Interval(10,22)
+		},
+		
+		{
+			interval : new Interval(21, 22),
+			result : [testedInterval, new Interval(10,22)]
+		},
+		{
+			interval : new Interval(8, 21),
+			result : new Interval(8,21)
+		},
+		
+		{
+			interval : new Interval(12, 19),
+			result : new Interval(10,20)
+		},
+		{
+			interval : new Interval(15, 16),
+			result : new Interval(10,20)
+		},
+		{
+			interval : new Interval(10, 20),
+			result : new Interval(10,20)
+		}
+    ].forEach(function (interval) {
+        it("should returns Interval[" + interval.result.start + ", " + interval.result.end + "] when doing union of " + testedInterval.toString() + " and " + interval.interval.toString(), function () {
+            var result = testedInterval.union(interval.interval);
+			expect(result.start).toEqual(interval.result.start);
+			expect(result.end).toEqual(interval.result.end);
+        });
+    });
+});
+
+describe("Interval - intersection", function () {
+    testedInterval = new Interval(10, 20);
+
+    [
+		{
+			interval : new Interval(8, 12),
+			result : new Interval(10,12)
+		},
+		{
+			interval : new Interval(17, 22),
+			result : new Interval(17,20)
+		},
+		
+		{
+			interval : new Interval(8, 21),
+			result : new Interval(10,20)
+		},
+		{
+			interval : new Interval(8, 9),
+			result : null
+		},
+		{
+			interval : new Interval(21, 22),
+			result : null
+		},
+		
+		{
+			interval : new Interval(12, 19),
+			result : new Interval(12,19)
+		},
+		{
+			interval : new Interval(15, 16),
+			result : new Interval(15,16)
+		},
+		{
+			interval : new Interval(10, 20),
+			result : new Interval(10,20)
+		}
+    ].forEach(function (interval) {
+		var msg;
+		var expectNull = false;
+		if (interval.result === null) {
+			msg = "should returns null when doing intersection of " + testedInterval.toString() + " and " + interval.interval.toString();
+			expectNull = true;
+		} else
+			msg = "should returns Interval[" + interval.result.start + ", " + interval.result.end + "] when doing intersection of " + testedInterval.toString() + " and " + interval.interval.toString();
+        it(msg, function () {
+            var result = testedInterval.intersection(interval.interval);
+			if (expectNull)
+				expect(result).toEqual(interval.result);
+			else {
+				expect(result.start).toEqual(interval.result.start);
+				expect(result.end).toEqual(interval.result.end);
+			}
+        });
+    });
+});
+
 
